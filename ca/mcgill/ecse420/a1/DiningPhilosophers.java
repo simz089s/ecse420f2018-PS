aa59e3276a046951d866069ff0596e25e644039f
Elvric <elvric.trombert@mail.mcgill.ca>
2018-09-26 14:14:26 -0400

    commited the file that did not go through

---
+++ ca/mcgill/ecse420/a1/DiningPhilosophers.java
@@ -0,0 +1,28 @@
+package ca.mcgill.ecse420.a1;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DiningPhilosophers {
+	
+	public static void main(String[] args) {
+
+		int numberOfPhilosophers = 5;
+                Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
+                Object[] chopsticks = new Object[numberOfPhilosophers];
+	}
+
+	public static class Philosopher implements Runnable {
+
+		
+
+		@Override
+		public void run() {
+			
+		}
+
+
+	}
+
+}
