5e912eeba31bd7c38b8339882f6cb8af81cd88b2
Simon Zheng <simz97@outlook.com>
2018-10-02 12:25:59 -0400

    Deadlocker continuation

--- src/ca/mcgill/ecse420/a1/Deadlocker.java
+++ src/ca/mcgill/ecse420/a1/Deadlocker.java
@@ -3,44 +3,35 @@ package ca.mcgill.ecse420.a1;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class Deadlocker {
-  private static Runnable th1;
-  private static Runnable th2;
-  private static boolean alive1 = false;
-  private static boolean alive2 = false;
-  private static Lock lock1 = new ReentrantLock();
-  private static Lock lock2 = new ReentrantLock();
+public class Deadlocker implements Runnable {
+
+  private static Lock lock = new ReentrantLock();
+
+  private int id;
 
   public static void main(String[] args) {
-    th1 =
-        () -> {
-          alive1 = true;
-          lock1.lock();
-          System.out.println("Thread 1 started.");
-          while (true) {
-            if (alive2) {
-              lock2.unlock();
-              lock1.unlock();
-              break;
-            }
-          }
-          System.out.println("Done.");
-        };
-    th2 =
-        () -> {
-          alive2 = true;
-          lock2.lock();
-          System.out.println("Thread 2 started.");
-          while (true) {
-            if (alive1) {
-              lock1.unlock();
-              lock2.unlock();
-              break;
-            }
-          }
-          System.out.println("Done.");
-        };
+    Runnable th1 = new Deadlocker(1);
+    Runnable th2 = new Deadlocker(2);
     th1.run();
     th2.run();
+  }
+
+  Deadlocker(int pId) {
+    id = pId;
+  }
+
+  @Override
+  public void run() {
+    System.out.println("Thread " + id + " started.");
+    while (true) {
+      lock.lock();
+      System.out.println("Sleeping.");
+      try {
+        Thread.sleep(1);
+      } catch (InterruptedException e) {
+      }
+      System.out.println("Done.");
+      lock.unlock();
+    }
   }
 }
