633929a287818a7947251574675936ea447ddb15
Elvric <elvric.trombert@mail.mcgill.ca>
2018-11-08 11:45:53 -0500

    Updated the code again

--- src/ca/mcgill/ecse420/a2/TestLocks.java
+++ src/ca/mcgill/ecse420/a2/TestLocks.java
@@ -18,11 +18,11 @@ public class TestLocks {
   public static void main(String[] args) {
     for (int i = 0; i < 1; i++) {
       System.out.println("Testing FilterLock");
-      TestLocks filterLocks = new TestLocks(new FilterLock(3), 1);
+      TestLocks filterLocks = new TestLocks(new FilterLock(50), 50);
       filterLocks.testTheLock();
 
       System.out.println("Testing BakeryLock");
-      TestLocks bakeryLock = new TestLocks(new LamportBakeryLock(2), 2);
+      TestLocks bakeryLock = new TestLocks(new LamportBakeryLock(50), 50);
       bakeryLock.testTheLock();
     }
   }
