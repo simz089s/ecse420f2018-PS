d183c4f4a901f02c7b115ae18397d5ccadafd7c9
Elvric <elvric.trombert@mail.mcgill.ca>
2018-11-09 17:02:03 -0500

    Removed the array counter + changed the anonymous class to lambda

--- src/ca/mcgill/ecse420/a2/TestLocks.java
+++ src/ca/mcgill/ecse420/a2/TestLocks.java
@@ -5,8 +5,8 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.locks.Lock;
 
 public class TestLocks {
-  //  private int counter = 0;
-  private static int counter = 0;
+  private int counter = 0;
+  // private static int counter = 0;
   private Lock lock;
   private int numThread;
 
@@ -16,37 +16,32 @@ public class TestLocks {
   }
 
   public static void main(String[] args) {
-      System.out.println("Testing FilterLock");
-      TestLocks filterLocks = new TestLocks(new FilterLock(50), 50);
-      filterLocks.testTheLock();
+    System.out.println("Testing FilterLock");
+    TestLocks filterLocks = new TestLocks(new FilterLock(50), 50);
+    filterLocks.testTheLock();
 
-      System.out.println("Testing BakeryLock");
-      TestLocks bakeryLock = new TestLocks(new LamportBakeryLock(50), 50);
-      bakeryLock.testTheLock();
+    System.out.println("Testing BakeryLock");
+    TestLocks bakeryLock = new TestLocks(new LamportBakeryLock(50), 50);
+    bakeryLock.testTheLock();
   }
 
   public void testTheLock() {
-    int[] cnt = {0};
     ExecutorService executor = Executors.newFixedThreadPool(numThread);
     for (int i = 0; i < numThread; i++) {
       executor.execute(
-          new Runnable() {
-            @Override
-            public void run() {
-              lock.lock();
-              counter++;
-              cnt[0]++;
-              lock.unlock();
-            }
+          () -> {
+            lock.lock();
+            counter++;
+            lock.unlock();
           });
     }
     executor.shutdown();
     while (!executor.isTerminated()) {}
-    if (cnt[0] == numThread) {
+    if (counter == numThread) {
       System.out.println("The Lock worked successfully");
     } else {
       System.out.println(
-          "The counter value was: " + cnt[0] + " but was expected to be: " + numThread);
+          "The counter value was: " + counter + " but was expected to be: " + numThread);
       throw new IllegalStateException();
     }
   }
