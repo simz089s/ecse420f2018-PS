8af51d1bdd74154ea0a85398d03250654b6523c5
Simon Zheng <simz97@outlook.com>
2018-11-27 21:42:11 -0500

    Implement matrix vector multiplication

---
+++ src/ca/mcgill/ecse420/a3/MergeMatrix.java
@@ -0,0 +1,133 @@
+package ca.mcgill.ecse420.a3;
+
+import java.util.Arrays;
+import java.util.concurrent.*;
+
+public class MergeMatrix {
+
+  public static final int MATRIX_SIZE = 20;
+
+  public static ExecutorService execs = Executors.newCachedThreadPool();
+
+  public static void sumUpRows(double[][] matrix, int top, int bot, int left, int right, double[] resultMatrix) {
+    if (bot - 1 == top) {
+      for (int i = left; i < right; i++) {
+        resultMatrix[top] += matrix[top][i];
+      }
+      return;
+    }
+
+    int midV = (bot - top) / 2 + top;
+    int midH = (right - left) / 2 + left;
+
+    Future<?> sumTop = execs.submit(() -> sumUpRows(matrix, top, midV, left, right, resultMatrix));
+    Future<?> sumBottom = execs.submit(() -> sumUpRows(matrix, midV, bot, left, right, resultMatrix));
+    try {
+      sumTop.get();
+      sumBottom.get();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void mergeMatrixProductSums(
+      double[][] matrix, double[] vector, int top, int bot, int left, int right, double[] resultMatrix) {
+    if (bot - 1 == top || right - 1 == left) {
+      for (int i = top; i < bot; i++) {
+        for (int j = left; j < right; j++) {
+          matrix[i][j] *= vector[j];
+        }
+      }
+      return;
+    }
+
+    int midV = (bot - top) / 2 + top;
+    int midH = (right - left) / 2 + left;
+
+    Future<?> mergeTopLeft =
+        execs.submit(() -> mergeMatrixProductSums(matrix, vector, top, midV, left, midH, resultMatrix));
+    Future<?> mergeTopRight =
+        execs.submit(() -> mergeMatrixProductSums(matrix, vector, top, midV, midH, right, resultMatrix));
+    Future<?> mergeBottomLeft =
+        execs.submit(() -> mergeMatrixProductSums(matrix, vector, midV, bot, left, midH, resultMatrix));
+    Future<?> mergeBottomRight =
+        execs.submit(() -> mergeMatrixProductSums(matrix, vector, midV, bot, midH, right, resultMatrix));
+    try {
+      mergeTopLeft.get();
+      mergeTopRight.get();
+      mergeBottomLeft.get();
+      mergeBottomRight.get();
+
+      // sumUpRows(matrix, top, midV, left, right, resultMatrix);
+      // sumUpRows(matrix, midV, bot, left, right, resultMatrix);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static double[] multiplyMatrix(double[][] matrix, double[] vector) {
+    double[][] matrixCopy = Arrays.copyOf(matrix, matrix.length);
+    double[] resultMatrix = new double[vector.length];
+    mergeMatrixProductSums(matrixCopy, vector, 0, matrix.length, 0, matrix[0].length, resultMatrix);
+    sumUpRows(matrix, 0, MATRIX_SIZE, 0, MATRIX_SIZE, resultMatrix);
+    return resultMatrix;
+  }
+
+  public static void main(String[] args) {
+    double[] v = generateRandomVector(MATRIX_SIZE);
+    double[][] m = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
+    System.out.println(
+        Arrays.toString(sequentialMultiplyMatrix(m, v))
+            .replace("], [", "],\n[")
+            .replace("[[", "[\n[")
+            .replace("]]", "]\n]"));
+    System.out.println(
+        Arrays.toString(multiplyMatrix(m, v))
+            .replace("], [", "],\n[")
+            .replace("[[", "[\n[")
+            .replace("]]", "]\n]"));
+    execs.shutdown();
+  }
+
+  private static double[][] generateRandomMatrix(int numRows, int numCols) {
+    double matrix[][] = new double[numRows][numCols];
+    for (int row = 0; row < numRows; row++) {
+      for (int col = 0; col < numCols; col++) {
+        matrix[row][col] = (double) ((int) (Math.random() * 10.0));
+      }
+    }
+    return matrix;
+  }
+
+  private static double[] generateRandomVector(int num) {
+    double vector[] = new double[num];
+    for (int row = 0; row < num; row++) {
+      vector[row] = (double) ((int) (Math.random() * 10.0));
+    }
+    return vector;
+  }
+
+  public static double[][] sequentialMultiplyMatrixNoAdd(double[][] matrix, double[] vector) {
+    double[][] result = new double[MATRIX_SIZE][MATRIX_SIZE];
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+      for (int j = 0; j < MATRIX_SIZE; j++) {
+        result[i][j] = matrix[i][j] * vector[j];
+      }
+    }
+    return result;
+  }
+
+  public static double[] sequentialMultiplyMatrix(double[][] matrix, double[] vector) {
+    double[] result = new double[MATRIX_SIZE];
+    for (int i = 0; i < MATRIX_SIZE; i++) {
+      for (int j = 0; j < MATRIX_SIZE; j++) {
+        result[i] += matrix[i][j] * vector[j];
+      }
+    }
+    return result;
+  }
+}
---
+++ src/ca/mcgill/ecse420/tests/BoundedLockBasedQueueTest.java
@@ -0,0 +1,8 @@
+package ca.mcgill.ecse420.tests;
+
+import ca.mcgill.ecse420.a3.BoundedLockBasedQueue;
+
+public class BoundedLockBasedQueueTest {
+  BoundedLockBasedQueue blbq10 = new BoundedLockBasedQueue(10);
+  BoundedLockBasedQueue blbq11 = new BoundedLockBasedQueue(11);
+}
